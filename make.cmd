49d58c10af509a1421579b695d64a43046decc0d
John Dylan <johndylan1995@gmail.com>
2025-01-02 15:45:00 +0700

    Update make file for v4

--- make.cmd
+++ make.cmd
@@ -4,19 +4,19 @@ set version=%2
 
 IF %func%==public (
     echo Start %func%:
-    git checkout main
+    git checkout v4
 
     git pull
 
-    git tag -a %version% -m "Releasing version v4.0.0"
+    git tag -a %version% -m "Releasing version v4.0.3"
 
     git push origin %version%
 
     SET GOPROXY=proxy.golang.org
 
-    go list -m github.com/doquangtan/gofiber-socket.io@%version%
+    go list -m github.com/doquangtan/gofiber-socket.io/v4@%version%
     
     echo Done %func%
 )
 
-@REM make public v4.0.0
+@REM make public v4.0.3
